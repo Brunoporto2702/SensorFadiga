0d7277dab42cc69c3df5e52ddf120eb6f3919306
Rebeca Moreno <RebecaMoreno@MacBook-Air-de-Rebeca.local>
2020-05-06 16:29:25 -0300

    atualizado nomes

--- get_features.py
+++ get_features.py
@@ -13,47 +13,43 @@ from imutils import face_utils
 from imutils.face_utils import FaceAligner
 from imutils.face_utils import rect_to_bb
 
-def eye_aspect_ratio(eye):
-    A = dist.euclidean(eye[1], eye[5])
-    B = dist.euclidean(eye[2], eye[4])
-    C = dist.euclidean(eye[0], eye[3])
+def distancia_dos_labios(formato_rosto):
+    labio_superior = formato_rosto[50:53]
+    labio_superior = np.concatenate((labio_superior, formato_rosto[61:64]))
+    labio_inferior = formato_rosto[56:59]
+    labio_inferior = np.concatenate((labio_inferior, formato_rosto[65:68]))
+    labio_superior_media = np.mean(labio_superior, axis=0)
+    labio_inferior_media = np.mean(labio_inferior, axis=0)
+    distancia = abs(labio_superior_media[1] - labio_inferior_media[1])
+    return distancia
+
+#calcula ear
+def calcula_ear_para_cada_olho(olho):
+    A = dist.euclidean(olho[1], olho[5])
+    B = dist.euclidean(olho[2], olho[4])
+    C = dist.euclidean(olho[0], olho[3])
     ear = (A + B) / (2.0 * C)
     return ear
 
-def lip_distance(shape):
-    top_lip = shape[50:53]
-    top_lip = np.concatenate((top_lip, shape[61:64]))
-    low_lip = shape[56:59]
-    low_lip = np.concatenate((low_lip, shape[65:68]))
-    top_mean = np.mean(top_lip, axis=0)
-    low_mean = np.mean(low_lip, axis=0)
-    distance = abs(top_mean[1] - low_mean[1])
-    return distance
-
-#calcula ear
-def final_ear(shape):
+def calcula_ear(formato_rosto):
     (lStart, lEnd) = imutils.face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
     (rStart, rEnd) = imutils.face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
-    leftEye = shape[lStart:lEnd]
-    rightEye = shape[rStart:rEnd]
-    leftEAR = eye_aspect_ratio(leftEye)
-    rightEAR = eye_aspect_ratio(rightEye)
-    ear = (leftEAR + rightEAR) / 2.0
-    return (ear, leftEye, rightEye)
-
+    olho_esquerdo = formato_rosto[lStart:lEnd]
+    olho_direito = formato_rosto[rStart:rEnd]
+    ear_olho_esquerdo = calcula_ear_para_cada_olho(olho_esquerdo)
+    ear_olho_direito = calcula_ear_para_cada_olho(olho_direito)
+    ear = (ear_olho_esquerdo + ear_olho_direito) / 2.0
+    return (ear, olho_esquerdo, olho_direito)
 
 def detecta_rosto(frame, predictor, detector):
     gray_video = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     retangulos_em_volta_da_face = detector(gray_video, 0)
     try:
         retangulo_face = retangulos_em_volta_da_face[0]
-
         formato_rosto = predictor(gray_video, retangulo_face)
         formato_rosto = face_utils.shape_to_np(formato_rosto)
-        olho = final_ear(formato_rosto)
-        ear, olho_esquerdo, olho_direito = olho[0], olho[1], olho[2]
-
-        distancia_entre_os_labios = lip_distance(formato_rosto)
+        ear, olho_esquerdo, olho_direito = calcula_ear(formato_rosto)
+        distancia_entre_os_labios = distancia_dos_labios(formato_rosto)
 
         rosto = {
             'ear': ear, 
@@ -62,13 +58,13 @@ def detecta_rosto(frame, predictor, detector):
             'distancia_entre_os_labios': distancia_entre_os_labios,
             'formato': formato_rosto
             }
-    except:
+    except Exception as e:
+        print(e)
         rosto = None
 
     return rosto
 
-
-def mostra_rosto(rosto, frame):
+def desenha_rosto(rosto, frame):
     mascara_olho_esquerdo = cv2.convexHull(rosto['olho_esquerdo']) #desenhar
     mascara_olho_direito = cv2.convexHull(rosto['olho_direito']) #desenhar
     cv2.drawContours(frame, [mascara_olho_esquerdo], -1, (0, 255, 0), 1) #desenhar
@@ -100,12 +96,16 @@ while True: # for frame in video
 
     #detecta rosto
     rosto = detecta_rosto(frame, predictor, detector)
+    #desenha rosto
     if rosto != None:
-        mostra_rosto(rosto, frame)
+        desenha_rosto(rosto, frame)
 
+    
+    #calcula features
     resultado.append(calcula_features(frame))
     
     cv2.imshow('Camera', frame)
     cv2.waitKey(1)
 
+    #exporta para o csv
     export_to_csv(resultado)
