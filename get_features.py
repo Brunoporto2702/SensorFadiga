d69e067c4ff7dd3338e176bf771b005c7ba75b75
Bruno Porto <brunop2702@gmail.com>
2020-05-06 18:59:12 -0300

    primeiras features calculadas e plotadas

--- get_features.py
+++ get_features.py
@@ -76,36 +76,134 @@ def calcula_features(frame):
     # retorna dicionario com as fetures por frame
     pass
 
-def export_to_csv(resultado):
+def export_to_csv(resultados):
     # exporta para CSV
     pass
+
+def calcula_ear_porcentagem(ear, max_olhoaberto, min_olhofechado):
+    ear_porcentagem = 100 * (ear - min_olhofechado)/(max_olhoaberto-min_olhofechado)
+    return ear_porcentagem
+
+def add_new_ear(lista_ear_atual,ear):
+    lista_ear_atual.append(round(ear,3))
+    lista_ear_atual = lista_ear_atual[1:3]
+    return lista_ear_atual
+
+def calcula_media(lista_ear_atual):
+    media = np.nanmean(lista_ear_atual)
+    return media
+
+def calcula_piscadas_por_min(lista_piscadas, num_frames_por_min):
+    if len(lista_piscadas) < num_frames_por_min:
+        num_frames_por_min = len(lista_piscadas)
+    
+    piscadas_no_ultimo_min = lista_piscadas[(len(lista_piscadas)- num_frames_por_min):] 
+
+    taxa_piscadas = (max(piscadas_no_ultimo_min) - min(piscadas_no_ultimo_min))
+
+    return taxa_piscadas
 
 detector = dlib.get_frontal_face_detector()  
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
 path_to_video = 'Dataset/02/0.mov'
 video = FileVideoStream(path_to_video).start()
+fps_video_rate = 25 # constante para o caso de vídeo
 
 # video = WebcamVideoStream(src=0).start()    #CAMERA DO PC
+fps = FPS().start()         #começa a contar o tempo de teste
 
-resultado = []
+resultados = []
 
-while True: # for frame in video 
+calibrado = False
+limite_frames_calibracao = 600
+
+lista_ear_calibracao = []
+lista_ear_atual = [0]*3
+YAWN_THRESH = 25
+
+piscando = False
+piscadas = 0
+piscadas_por_min = []
+lista_piscadas = []
+
+frames = []
+
+# while True: # for frame in video 
+for i in range(5000):
     frame = video.read()
     frame = imutils.resize(frame, width=300, height=300)
 
-    #detecta rosto
-    rosto = detecta_rosto(frame, predictor, detector)
-    #desenha rosto
+    rosto = detecta_rosto(frame, predictor, detector)  #detecta rosto
+   
     if rosto != None:
-        desenha_rosto(rosto, frame)
+        # desenha_rosto(rosto, frame)  #desenha rosto
+        ear = rosto['ear']
 
+    lista_ear_atual = add_new_ear(lista_ear_atual,ear)
+    ear_filtrado = calcula_media(lista_ear_atual)
     
-    #calcula features
-    resultado.append(calcula_features(frame))
-    
-    cv2.imshow('Camera', frame)
-    cv2.waitKey(1)
 
-    #exporta para o csv
-    export_to_csv(resultado)
+    if fps._numFrames < limite_frames_calibracao:
+        lista_ear_calibracao.append(ear_filtrado)
+        max_olhoaberto = np.median(lista_ear_calibracao)
+        min_olhofechado = min(lista_ear_calibracao)
+        EYE_AR_THRESH = 0.5*max_olhoaberto
+    
+    elif fps._numFrames >= limite_frames_calibracao:
+        #calcula features
+        ear_porcentagem = calcula_ear_porcentagem(ear_filtrado, max_olhoaberto, min_olhofechado)
+
+        if ear_filtrado < EYE_AR_THRESH and not piscando:
+            piscadas += 1
+            piscando = True
+        elif ear_filtrado > EYE_AR_THRESH and piscando:
+            piscando = False
+
+        num_frames_por_min = 60*fps_video_rate
+
+        if fps._numFrames % num_frames_por_min == 0:
+            piscadas_por_min.append(piscadas)
+        
+        lista_piscadas.append(piscadas)
+        piscadas_por_min_atual = calcula_piscadas_por_min(lista_piscadas, num_frames_por_min)
+
+        print('Piscadas total: {}'.format(piscadas))
+        print('lista piscadas por min: {}'.format(piscadas_por_min))
+        print('piscadas por min atual: {}'.format(piscadas_por_min_atual))
+        print('ear%: {}'.format(ear_porcentagem))
+        print('ear: {}'.format(ear_filtrado))
+        print('\n\n')
+
+
+        resultado = {
+            'ear_porcentagem': ear_porcentagem, 
+            'ear_filtrado': ear_filtrado, 
+            'ear': ear,
+            'EYE_AR_THRESH': EYE_AR_THRESH,
+            'piscadas':piscadas,
+            'piscadas_por_min_atual': piscadas_por_min_atual,
+            }
+        resultados.append(resultado) 
+        
+
+
+        
+
+        
+    # cv2.imshow('Camera', frame)
+    # cv2.waitKey(1)
+    fps.update()
+    frames.append(fps._numFrames)
+            
+
+df = pd.DataFrame(resultados)
+for index, coluna in enumerate(df.columns):
+    plt.subplot(2,3,index+1)
+    plt.plot(range(len(df[coluna].values)), df[coluna].values)
+    plt.title(coluna)
+
+plt.show()
+    
+#exporta para o csv    
+export_to_csv(resultados)
