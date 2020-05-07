fb161c40d5b4425808aa997c37402fba02fcf2aa
Rebeca Moreno <RebecaMoreno@MacBook-Air-de-Rebeca.local>
2020-05-07 16:57:52 -0300

    filtro de reconstruction no EAR com Ayres

--- get_features.py
+++ get_features.py
@@ -23,7 +23,6 @@ def distancia_dos_labios(formato_rosto):
     distancia = abs(labio_superior_media[1] - labio_inferior_media[1])
     return distancia
 
-#calcula ear
 def calcula_ear_para_cada_olho(olho):
     A = dist.euclidean(olho[1], olho[5])
     B = dist.euclidean(olho[2], olho[4])
@@ -42,14 +41,32 @@ def calcula_ear(formato_rosto):
     return (ear, olho_esquerdo, olho_direito)
 
 def detecta_rosto(frame, predictor, detector):
-    gray_video = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    retangulos_em_volta_da_face = detector(gray_video, 0)
+    retangulos_em_volta_da_face = detector.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=10, minSize=(50, 50),flags=cv2.CASCADE_SCALE_IMAGE)
+
+    #verifica se precisa rotacionar
+    if len(retangulos_em_volta_da_face) == 0:
+        frame = cv2.putText(frame, "falha", (10,10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        frame = imutils.rotate(frame, 270)
+        retangulos_em_volta_da_face = detector.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=10, minSize=(50, 50),flags=cv2.CASCADE_SCALE_IMAGE)
+   
+    #detecta o rosto
     try:
-        retangulo_face = retangulos_em_volta_da_face[0]
-        formato_rosto = predictor(gray_video, retangulo_face)
+        #selecionar roi - com haar cascade
+        (x,y,w,h) = retangulos_em_volta_da_face[0] #posicao rosto detectado
+        frame = frame[y:y+h,x:x+w] #corta frame
+        frame = imutils.resize(frame, width=300, height=300) #resize frame
+        retangulo_face = dlib.rectangle(0, 0, 300, 300) #pega o tamanho 
+        
+        #sem selecionar roi - com haar cascade
+        # (x,y,w,h) = retangulos_em_volta_da_face[0]
+        # retangulo_face = dlib.rectangle(x, y, x+w, y+h)
+
+        formato_rosto = predictor(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), retangulo_face)
         formato_rosto = face_utils.shape_to_np(formato_rosto)
-        ear, olho_esquerdo, olho_direito = calcula_ear(formato_rosto)
-        distancia_entre_os_labios = distancia_dos_labios(formato_rosto)
+
+        #dectecta infos
+        ear, olho_esquerdo, olho_direito = calcula_ear(formato_rosto) #olhos
+        distancia_entre_os_labios = distancia_dos_labios(formato_rosto) #boca
 
         rosto = {
             'ear': ear, 
@@ -58,11 +75,12 @@ def detecta_rosto(frame, predictor, detector):
             'distancia_entre_os_labios': distancia_entre_os_labios,
             'formato': formato_rosto
             }
+
     except Exception as e:
         print(e)
         rosto = None
 
-    return rosto
+    return frame, rosto
 
 def desenha_rosto(rosto, frame):
     mascara_olho_esquerdo = cv2.convexHull(rosto['olho_esquerdo']) #desenhar
@@ -72,21 +90,24 @@ def desenha_rosto(rosto, frame):
     labio = rosto['formato'][48:60] #desenhar
     cv2.drawContours(frame, [labio], -1, (0, 255, 0), 1) #desenhar
 
-def calcula_features(frame):
-    # retorna dicionario com as fetures por frame
-    pass
+def export_to_csv(df):
+    # exporta para xlsx
+    from pyexcelerate import Workbook
 
-def export_to_csv(resultados):
-    # exporta para CSV
-    pass
+    values = [df.columns] + list(df.values)
+    wb = Workbook()
+    wb.new_sheet('df', data=values)
+    wb.save('df.xlsx')
+    
 
 def calcula_ear_porcentagem(ear, max_olhoaberto, min_olhofechado):
     ear_porcentagem = 100 * (ear - min_olhofechado)/(max_olhoaberto-min_olhofechado)
     return ear_porcentagem
 
-def add_new_ear(lista_ear_atual,ear):
+def add_new_ear(lista_ear_atual,ear):    
     lista_ear_atual.append(round(ear,3))
-    lista_ear_atual = lista_ear_atual[1:3]
+    lista_ear_atual = lista_ear_atual[1:4]
+    print(len(lista_ear_atual))
     return lista_ear_atual
 
 def calcula_media(lista_ear_atual):
@@ -96,14 +117,11 @@ def calcula_media(lista_ear_atual):
 def calcula_piscadas_por_min(lista_piscadas, num_frames_por_min):
     if len(lista_piscadas) < num_frames_por_min:
         num_frames_por_min = len(lista_piscadas)
-    
-    piscadas_no_ultimo_min = lista_piscadas[(len(lista_piscadas)- num_frames_por_min):] 
+    piscadas_no_ultimo_min = lista_piscadas[(len(lista_piscadas) - num_frames_por_min):] 
+    taxa_piscadas_por_min = (max(piscadas_no_ultimo_min) - min(piscadas_no_ultimo_min))
+    return taxa_piscadas_por_min 
 
-    taxa_piscadas = (max(piscadas_no_ultimo_min) - min(piscadas_no_ultimo_min))
-
-    return taxa_piscadas
-
-detector = dlib.get_frontal_face_detector()  
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
 path_to_video = 'Dataset/02/0.mov'
@@ -127,75 +145,71 @@ piscadas = 0
 piscadas_por_min = []
 lista_piscadas = []
 
-frames = []
 
 # while True: # for frame in video 
-for i in range(5000):
+for i in range(4000):
     frame = video.read()
     frame = imutils.resize(frame, width=300, height=300)
 
-    rosto = detecta_rosto(frame, predictor, detector)  #detecta rosto
-   
+    frame, rosto = detecta_rosto(frame, predictor, detector)  #detecta rosto
     if rosto != None:
-        # desenha_rosto(rosto, frame)  #desenha rosto
+        desenha_rosto(rosto, frame)  #desenha rosto
         ear = rosto['ear']
 
-    lista_ear_atual = add_new_ear(lista_ear_atual,ear)
-    ear_filtrado = calcula_media(lista_ear_atual)
-    
-
-    if fps._numFrames < limite_frames_calibracao:
-        lista_ear_calibracao.append(ear_filtrado)
-        max_olhoaberto = np.median(lista_ear_calibracao)
-        min_olhofechado = min(lista_ear_calibracao)
-        EYE_AR_THRESH = 0.5*max_olhoaberto
-    
-    elif fps._numFrames >= limite_frames_calibracao:
-        #calcula features
-        ear_porcentagem = calcula_ear_porcentagem(ear_filtrado, max_olhoaberto, min_olhofechado)
-
-        if ear_filtrado < EYE_AR_THRESH and not piscando:
-            piscadas += 1
-            piscando = True
-        elif ear_filtrado > EYE_AR_THRESH and piscando:
-            piscando = False
-
-        num_frames_por_min = 60*fps_video_rate
-
-        if fps._numFrames % num_frames_por_min == 0:
-            piscadas_por_min.append(piscadas)
-        
-        lista_piscadas.append(piscadas)
-        piscadas_por_min_atual = calcula_piscadas_por_min(lista_piscadas, num_frames_por_min)
-
-        print('Piscadas total: {}'.format(piscadas))
-        print('lista piscadas por min: {}'.format(piscadas_por_min))
-        print('piscadas por min atual: {}'.format(piscadas_por_min_atual))
-        print('ear%: {}'.format(ear_porcentagem))
-        print('ear: {}'.format(ear_filtrado))
-        print('\n\n')
-
-
-        resultado = {
-            'ear_porcentagem': ear_porcentagem, 
-            'ear_filtrado': ear_filtrado, 
-            'ear': ear,
-            'EYE_AR_THRESH': EYE_AR_THRESH,
-            'piscadas':piscadas,
-            'piscadas_por_min_atual': piscadas_por_min_atual,
-            }
-        resultados.append(resultado) 
+        lista_ear_atual = add_new_ear(lista_ear_atual,ear)
+        ear_filtrado = calcula_media(lista_ear_atual)
         
 
-
+        if fps._numFrames < limite_frames_calibracao:
+            lista_ear_calibracao.append(ear_filtrado)
+            max_olhoaberto = np.median(lista_ear_calibracao)
+            min_olhofechado = min(lista_ear_calibracao)
+            EYE_AR_THRESH = 0.7*max_olhoaberto
         
+        elif fps._numFrames >= limite_frames_calibracao:
+            #calcula features
+            ear_porcentagem = calcula_ear_porcentagem(ear_filtrado, max_olhoaberto, min_olhofechado)
 
-        
-    # cv2.imshow('Camera', frame)
-    # cv2.waitKey(1)
-    fps.update()
-    frames.append(fps._numFrames)
+            if ear_filtrado < EYE_AR_THRESH and not piscando:
+                piscadas += 1
+                piscando = True
+            elif ear_filtrado > EYE_AR_THRESH and piscando:
+                piscando = False
+
+            num_frames_por_min = 60*fps_video_rate
+
+            if fps._numFrames % num_frames_por_min == 0:
+                piscadas_por_min.append(piscadas)
             
+            lista_piscadas.append(piscadas)
+            piscadas_por_min_atual = calcula_piscadas_por_min(lista_piscadas, num_frames_por_min)
+
+            print('Piscadas total: {}'.format(piscadas))
+            print('lista piscadas por min: {}'.format(piscadas_por_min))
+            print('piscadas por min atual: {}'.format(piscadas_por_min_atual))
+            print('ear %: {}'.format(ear_porcentagem))
+            print('ear: {}'.format(ear_filtrado))
+            print('\n\n')
+
+
+            resultado = {
+                'ear_porcentagem': ear_porcentagem, 
+                'ear_filtrado': ear_filtrado, 
+                'ear': ear,
+                'EYE_AR_THRESH': EYE_AR_THRESH,
+                'piscadas':piscadas,
+                'piscadas_por_min_atual': piscadas_por_min_atual,
+                }
+            resultados.append(resultado) 
+        
+
+
+        
+
+        
+    cv2.imshow('Camera', frame)
+    cv2.waitKey(1)
+    fps.update()
 
 df = pd.DataFrame(resultados)
 for index, coluna in enumerate(df.columns):
@@ -206,4 +220,4 @@ for index, coluna in enumerate(df.columns):
 plt.show()
     
 #exporta para o csv    
-export_to_csv(resultados)
+export_to_csv(df)
