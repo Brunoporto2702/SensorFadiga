94391d2562bd9c44365d75e7316c210920c78b1d
Rebeca Moreno <RebecaMoreno@MacBook-Air-de-Rebeca.local>
2020-05-13 12:54:30 -0300

    tudo certo

--- get_features.py
+++ get_features.py
@@ -81,6 +81,13 @@ def detecta_rosto(frame, predictor, detector):
 
     return frame, rosto
 
+def plot_graph(df):
+    for index, coluna in enumerate(df.columns):
+        plt.subplot(2,1,index+1)
+        plt.plot(range(len(df[coluna].values)), df[coluna].values)
+        plt.title(coluna)
+    plt.show()
+
 def desenha_rosto(rosto, frame):
     mascara_olho_esquerdo = cv2.convexHull(rosto['olho_esquerdo']) #desenhar
     mascara_olho_direito = cv2.convexHull(rosto['olho_direito']) #desenhar
@@ -95,63 +102,46 @@ def exporta_para_xlsx(df):
     wb.new_sheet('df', data=values)
     wb.save('df.xlsx')
 
-<<<<<<< HEAD
-detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
-
-path_to_video = 'Dataset/02/0.mov'
-video = FileVideoStream(path_to_video).start()
-
-resultados = []
-try:
-    while True: # for frame in video 
-        frame = video.read()
-        frame = imutils.resize(frame, width=300, height=300)
-
-        frame, rosto = detecta_rosto(frame, predictor, detector)  #detecta rosto
-        if rosto != None:
-            # desenha_rosto(rosto, frame)  #desenha rosto
-            ear = rosto['ear']
-            distancia_entre_os_labios = rosto['distancia_entre_os_labios']
-        else:
-            ear = -1
-            distancia_entre_os_labios = -1
-
-        print('ear: {}'.format(ear))
-        print('distancia_entre_os_labios: {}'.format(distancia_entre_os_labios))
-        print('\n\n')
-
-        resultado = {
-            'ear': ear,
-            'distancia_entre_os_labios': distancia_entre_os_labios
-            }
-=======
-def plot_graph(df):
-    for index, coluna in enumerate(df.columns):
-        plt.subplot(2,1,index+1)
-        plt.plot(range(len(df[coluna].values)), df[coluna].values)
-        plt.title(coluna)
-    plt.show()
- 
 def arquivo_features(path_to_video, nome_video):
     detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
     predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
->>>>>>> 12a8b3d6866fdebb58fe423e8077c51de987edd9
+    
+    video = FileVideoStream(path_to_video).start()
 
-        resultados.append(resultado) 
-            
-        # cv2.imshow('Camera', frame)
-        # cv2.waitKey(1)
+    resultados = []
+    try:
+        while True: # for frame in video 
+            frame = video.read()
+            frame = imutils.resize(frame, width=300, height=300)
 
-except Exception as e:
-    print(e)
-    traceback.print_exc()
+            frame, rosto = detecta_rosto(frame, predictor, detector)  #detecta rosto
+            if rosto != None:
+                # desenha_rosto(rosto, frame)  #desenha rosto
+                ear = rosto['ear']
+                distancia_entre_os_labios = rosto['distancia_entre_os_labios']
+            else:
+                ear = -1
+                distancia_entre_os_labios = -1
 
-df = pd.DataFrame(resultados)
-for index, coluna in enumerate(df.columns):
-    plt.subplot(2,1,index+1)
-    plt.plot(range(len(df[coluna].values)), df[coluna].values)
-    plt.title(coluna)
-plt.show()
+            print('ear: {}'.format(ear))
+            print('distancia_entre_os_labios: {}'.format(distancia_entre_os_labios))
+            print('\n\n')
 
-exporta_para_xlsx(df)
+            resultado = {
+                'ear': ear,
+                'distancia_entre_os_labios': distancia_entre_os_labios
+                }
+
+    
+
+            resultados.append(resultado) 
+                
+            # cv2.imshow('Camera', frame)
+            # cv2.waitKey(1)
+
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
+
+    df = pd.DataFrame(resultados)
+    return df
